6c513276425c22c50e1ce8fe2189c198572ae584
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-11-04 20:00:13 +0100

    modified  app/api/todo/route.ts

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     //fetch all todos in the db and order by createdAt in desc order
